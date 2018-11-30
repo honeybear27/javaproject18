c8b8451011615466d23cec9a14d6fb6d34ea8540
Ronald <xelloss@vt.edu>
2018-11-30 02:36:00 -0500

    completed airObject

--- LRURunway/src/lrurunway/AirObject.java
+++ LRURunway/src/lrurunway/AirObject.java
@@ -1,5 +1,37 @@
 package lrurunway;
 
 public class AirObject {
+	private String name;
+	private String company;
+	
+	public AirObject (String name, String company) {
+		this.name = name;
+		this.company = company;
+		
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String newName) {
+		if (!newName.equals("")) {
+			name = newName;
+		}
+	}
+	
+	public String getCompany() {
+		return company;
+	}
+	
+	public void setCompany(String newCompany) {
+		if (!newCompany.equals("")) {
+			company = newCompany;
+		}
+	}
+	
+	public String toString() {
+		return "AirObject : [" + name + "] [" + company + "]";
+	}
 
 }
